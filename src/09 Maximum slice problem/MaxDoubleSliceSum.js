a186f1f52bcb80ae5aa43d7bf9673f26f51ecf1f
Predjo <predovan.nikola@gmail.com>
2019-03-27 01:29:55 +0100

    add comment

--- src/09 Maximum slice problem/MaxDoubleSliceSum.js
+++ src/09 Maximum slice problem/MaxDoubleSliceSum.js
@@ -6,6 +6,7 @@ export function solution(A) {
   let right = Array(N).fill(0);
 
   for (let i = 1; i < N - 1; i++) {
+    // max(x, 0) because empty slice (X, Y, Z) == (Y - 1, Y, Y + 1) == 0
     left[i] = Math.max(left[i - 1] + A[i], 0);
     right[N - 1 - i] = Math.max(right[N - i] + A[N - 1 - i], 0);
   }
